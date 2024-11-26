cb129364c0947952eede27add5f50c1e8d4a1b67
Shubham <shub88510@gmail.com>
2024-11-26 19:05:17 +0530

    Added

---
+++ practice.js
@@ -0,0 +1,19 @@
+// const product = {
+//     title: "Ball Pen",
+//     rating: 4,
+//     offer: 5,
+//     price: 270,
+// };
+
+// console.log(product);
+
+const profile = {
+    username: "Shubham",
+    isFollow: true,
+    post: 200,
+    following: 3000000,
+    followers: 10000000,
+}
+
+console.log(profile);
+console.log(typeof profile["username"]);
